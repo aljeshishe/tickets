521c57adf213af3d7074f1f93c23215c053e91cc
alexey.grachev <alexey.grachev@nordigy.ru>
2018-07-03 15:53:40 +0300

    added 41 cities to tickets

--- tickets.py
+++ tickets.py
@@ -1,16 +1,11 @@
 import itertools
-import logging
 import logging.config
-from datetime import date
+from datetime import date, datetime
 from functools import partial
-
-import db
-import model
 import proxied_requests
 from model import task, SQLALCHEMY_DATABASE_NAME
 from processor import Processor
-from results import *
-from utils import date_range
+from utils import date_range, as_list
 
 log = logging.getLogger(__name__)
 __author__ = 'alexey.grachev'
@@ -65,14 +60,55 @@ log_config = {
     }
 }
 
-
+airports ='''AGP
+ALC
+AMS
+ARN
+BCN
+BGO
+BGY
+BIO
+BOJ
+BRE
+BRI
+BUD
+CGN
+CIA
+CPH
+CRL
+DUB
+DUS
+FMO
+FRA
+GVA
+HAJ
+HEL
+IEV
+LEJ
+MAD
+MLA
+MUC
+OSL
+PSA
+STN
+STR
+SVQ
+SXF
+TLL
+VAR
+VIE
+VKO
+VLC
+VNO
+WAW'''
 if __name__ == '__main__':
     logging.config.dictConfig(log_config)
     airports = ['BERL', 'BOJ', 'BRE', 'BTS', 'BUD', 'CGN', 'DEB', 'DUS', 'HAJ', 'HAM', 'LED', 'LTN', 'MILA', 'MOSC', 'MUC', 'PARI', 'PMI', 'STR', 'TLL', 'RIX', 'VNO', 'VIE', 'CIA']
+    # airports = airports.split()
     tasks = generator(task=task,
                       depart=airports,
                       arrive=airports,
-                      date=date_range(date(2018, 10, 16),6),
+                      date=date_range(date(2018, 10, 15), 5),
                       requests=proxied_requests.Requests())
-    p = Processor(10)
+    p = Processor(5)
     p.run(tasks)
